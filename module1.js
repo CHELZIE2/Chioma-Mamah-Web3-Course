5e507ff8e0f3298570297668fe60f29b562fb218
CHELZIE2 <164629142+CHELZIE2@users.noreply.github.com>
2024-11-09 00:22:30 +0100

    Update module1.js

--- module1.js
+++ module1.js
@@ -1,1 +1,21 @@
+// Looping a triangle
+let character = "#"
+for(N = 1 ; N < 8 ; N++){
+  let result = character.repeat(N)
+  console.log(result)
+}
 
+// FizzBuzz
+let numberz = 1
+while(numberz <= 100){
+  if(numberz % 3 == 0 && numberz % 5 == 0){
+    console.log("FizzBuzz")
+  }else if(numberz % 3 == 0){
+    console.log("Fizz")
+  }else if(numberz % 5 == 0){
+    console.log("Buzz")
+  }else{
+    console.log(numberz)
+  }
+  numberz ++
+}
