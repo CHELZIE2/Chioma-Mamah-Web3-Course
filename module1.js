097d56513e6a9391c19fb7f31123dca326d0154e
CHELZIE2 <164629142+CHELZIE2@users.noreply.github.com>
2024-11-10 21:58:36 +0100

    Update module1.js

--- module1.js
+++ module1.js
@@ -32,7 +32,62 @@ for (let row = 0; row < size; row++) {      // Outer loop for rows
       board += " ";  // Add space if row + col is odd
     }
   }
-  board += "\n";  // After each row, add a newline character
+board += "\n";  // After each row, add a newline character
 }
 
 console.log(board);
+
+// Minimum Function
+  function min(a, b) {
+  if (a < b) {
+    return a;
+  } else if (a > b) {
+    return b;
+  } else {
+    return 'they are equal';
+  }
+}
+
+console.log(min(0, 10))
+console.log(min(0, -10))
+console.log(min(93, 93))
+
+// Recursion
+function isEven(n) {
+  // Handle negative numbers by converting them to positive
+  if (n < 0) {
+    return isEven(-n);
+  }
+  // Base cases
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  }
+  // Recursive case
+  else {
+    return isEven(n - 2);
+  }
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+
+
+
+// Bean Counting
+function countBs(str) {
+  return countChar(str, 'B'); 
+}
+function countChar(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countBs("BOB"));
+console.log(countChar("kakkerlak", "k"));
